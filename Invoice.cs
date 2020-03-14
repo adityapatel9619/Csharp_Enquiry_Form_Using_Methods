206bc0f4fd2416108b7de9ecf0bf5f11a89f264d
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2020-03-15 01:32:17 +0530

    Add project files.

---
+++ Invoice.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace entry
+{
+    class Invoice
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("********Enquiry Form********");
+            ///<summary>
+            ///This section is calling methods by creating objects or instance
+            /// </summary>
+            Program p = new Program();
+            p.enquriy();
+            p.print();
+
+
+
+
+            ///This section is under development phase
+            ///<summary>
+            ///This section will work after submittng the for systemm will ask for one more chance for new entries
+            /// </summary>
+
+            /*int a;
+            Console.WriteLine("Do you wnat to enter");
+            a = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                p.enquriy();
+                p.print();
+
+            } while (a == 1);
+
+            if (a == 0)
+            {
+                System.Environment.Exit(0);
+            }*/
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace entry
+{
+    class Program
+    {
+        /// <summary>
+        /// *******Enquiry Form*******
+        /// This program is an simple enquiry form made using class and methods
+        /// </summary>
+
+        //***Variable declartion***
+        public string name,fathername,mothername,email,course, phone;
+        public int i,n;
+
+        //***Class Enquiry***
+        /// <summary>
+        /// This class takes input from the user
+        /// </summary>
+        public void enquriy()
+        {
+            //This code ask how many enquiry user wants to enter
+            Console.WriteLine("How Many Enteries You Want To Fill(*Note if entered 0 program will terminate):");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            //for loop used here for multiple and single entries as well 
+            for (i = 0; i < n; i++) { 
+            Console.Write("Enter Name:");
+            name = Console.ReadLine();
+            Console.Write("Enter Fathername:");
+            fathername = Console.ReadLine();
+            Console.Write("Enter Mothername:");
+            mothername = Console.ReadLine();
+            Console.Write("Email:");
+            email = Console.ReadLine();
+            Console.Write("Enter Course Name:");
+            course = Console.ReadLine();
+            Console.Write("Enter Phone Number:");
+            phone =Console.ReadLine();
+                Console.WriteLine("\n");
+            }
+
+            //***Program exit***
+            ///<summary>
+            ///If user enters 0 the program will terminate
+            /// </summary>
+            if (n == 0)
+            {
+                System.Environment.Exit(0);
+                
+            }
+        }
+
+        //***For printing the input values***
+        /// <summary>
+        /// This program diplays the user input value to admin in invoice format
+        /// </summary>
+        public void print()
+        {
+            Console.WriteLine("\n");
+            Console.WriteLine("*******Invoice*******");
+            for (i = 0; i <n; i++) { 
+            Console.WriteLine(name);
+            Console.WriteLine(fathername);
+            Console.WriteLine(mothername);
+            Console.WriteLine(email);
+            Console.WriteLine(phone);
+            Console.WriteLine(course);
+                Console.WriteLine("");
+                Console.WriteLine("Thanks! We will Contact You Soon ");
+               
+                
+            }
+            
+        }
+       
+    }
+}
